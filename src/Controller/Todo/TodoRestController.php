b870ade6632da93e983a7fdc1e1dd4cba0663e96
marina-a90 <marinaandjelkovic456@gmail.com>
2019-07-03 10:51:17 +0200

    fixtures

--- src/Controller/Todo/TodoRestController.php
+++ src/Controller/Todo/TodoRestController.php
@@ -3,63 +3,65 @@
 namespace App\Controller\Todo;
 
 use App\Entity\Todo\Todo;
-use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\View\View;
 //use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Swagger\Annotations as SWG;
 
-class TodoRestController extends AbstractFOSRestController
+class TodoRestController extends FOSRestController
 {
-//    /**
-//     * Get list of Todos.
-//     *
-//     * @Route("/todos", methods={"GET"})
-//     *
-//     * @SWG\Get(
-//     *     tags={"Todo"},
-//     *     summary="Get list of Todos",
-//     *     description="Get list of Todos",
-//     *     produces={"application/json"},
-//     *     @SWG\Tag(name="Todo"),
-//     *     @SWG\Response(
-//     *         response=200, description="Success"
-//     *     ),
-//     *     @SWG\Response(
-//     *         response=204, description="No content"
-//     *     )
-//     *  )
-//     *
-//     * @return View
-//     */
-//    public function getTodosAction()
-//    {
-//        $todos = $this->getDoctrine()
-//            ->getRepository(Todo::class)
-//            ->findAll()
-//        ;
-//
-//        return $this->getViewHandler($this->view($todos));
-////        return $this->handleView($this->view($todos));
-//    }
-
-
     /**
-     * Create todo.
-     * @Route("/todo", methods={"POST"})
+     * Get list of Todos.
      *
-     * @return array
+     * @Route("/todos", methods={"GET"})
+     *
+     * @SWG\Get(
+     *     tags={"Todo"},
+     *     summary="Get list of Todos",
+     *     description="Get list of Todos",
+     *     produces={"application/json"},
+     *     @SWG\Tag(name="Todo"),
+     *     @SWG\Response(
+     *         response=200, description="Success"
+     *     ),
+     *     @SWG\Response(
+     *         response=204, description="No content"
+     *     )
+     *  )
+     *
+//     * @return View
      */
-    public function postTodoAction(Request $request)
+    public function getTodosAction()
     {
-        $todo = new todo();
-        $todo->setTodo('first todo');
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($todo);
-        $em->flush();
-        return View::create($todo, Response::HTTP_CREATED , []);
+        $todos = $this->getDoctrine()
+            ->getRepository(Todo::class)
+            ->findAll()
+        ;
 
+        return new Response($this->json($todos), Response::HTTP_OK);
+//      return $this->view($todos, Response::HTTP_OK);
     }
+//
+//
+//    /**
+//     * Create todo.
+//     * @Route("/todo", methods={"POST"})
+//     *
+//     * @return array
+//     */
+//    public function postTodoAction(Request $request)
+//    {
+//        $todo = new todo();
+//        $todo->setTodo('first todo');
+//        $em = $this->getDoctrine()->getManager();
+//        $em->persist($todo);
+//        $em->flush();
+////        return View::create($todo, Response::HTTP_CREATED , []);
+//
+//    }
 
 }
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Todo\Todo;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+//
+//        $manager->flush();
+
+        for ($i = 0; $i < 10; $i++) {
+            $todo = new Todo();
+            $todo->setTodo('todo '.$i);
+            $todo->setIsDone(mt_rand(0, 1));
+            $manager->persist($todo);
+        }
+
+        $manager->flush();
+    }
+}
